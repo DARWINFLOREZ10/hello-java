d3888dc947a9ebec9ce43f326d0bca9bd208baf8
Darwin Cardenas Florez <darwinflorezcardenas@gmail.com>
2025-12-17 10:24:55 -0500

    Update AccessModifiers.java

--- basic/c08_oop/AccessModifiers.java
+++ basic/c08_oop/AccessModifiers.java
@@ -1,10 +1,5 @@
 package basic.c08_oop;
 
-/*
-Clase 59 - Modificadores de acceso
-Vídeo: https://youtu.be/JOAqpdM36wI?t=21542
-*/
-
 public class AccessModifiers {
 
     public static void main(String[] args) {
