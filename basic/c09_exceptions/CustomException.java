205071ea42d82cf1d90fa84030eb09a9819929e8
Darwin Cardenas Florez <darwinflorezcardenas@gmail.com>
2025-12-17 10:27:27 -0500

    Update CustomException.java

--- basic/c09_exceptions/CustomException.java
+++ basic/c09_exceptions/CustomException.java
@@ -1,10 +1,5 @@
 package basic.c09_exceptions;
 
-/*
-Clase 71 - Excepciones personalizadas
-Vídeo: https://youtu.be/JOAqpdM36wI?t=29181
-*/
-
 public class CustomException extends Exception {
 
     public CustomException(String message) {
