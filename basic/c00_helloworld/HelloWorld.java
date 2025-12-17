86ad7d34d8e822225b3496f1872fda2db8de1243
Darwin Cardenas Florez <darwinflorezcardenas@gmail.com>
2025-12-17 10:17:30 -0500

    Update HelloWorld.java

--- basic/c00_helloworld/HelloWorld.java
+++ basic/c00_helloworld/HelloWorld.java
@@ -1,13 +1,5 @@
 package basic.c00_helloworld;
 
-/*
-Clase 12 - Hola Mundo
-Vídeo: https://youtu.be/JOAqpdM36wI?t=4250
-
-Clase 13 - Comentarios
-Vídeo: https://youtu.be/JOAqpdM36wI?t=4937
-*/
-
 // Hola Mundo
 
 public class HelloWorld {
